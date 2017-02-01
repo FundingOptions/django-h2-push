5fd4fe81869014c59827327dfdf55783d819d959
Anthony King <anthony.king@fundingoptions.com>
2017-02-01 19:13:43 +0000

    add base project

---
+++ static_preload/__init__.py
@@ -0,0 +1,1 @@
+default_app_config = 'static_preload.apps.StaticPreloadConfig'
---
+++ static_preload/apps.py
@@ -0,0 +1,5 @@
+from django.apps import AppConfig
+
+
+class StaticPreloadConfig(AppConfig):
+    name = 'static_preload'
